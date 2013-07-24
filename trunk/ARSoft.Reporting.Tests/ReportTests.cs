73bdf97811ab97b394eaa52f3041c4fafb428100
Marcos Mellibovsky <mellibo@gmail.com>
2013-07-24 16:56:18 -0300

    first test

---
+++ trunk/ARSoft.Reporting.Tests/ReportTests.cs
@@ -0,0 +1,34 @@
+﻿namespace ARSoft.Reporting.Tests
+{
+    using NUnit.Framework;
+
+    using SharpTestsEx;
+
+    [TestFixture]
+    public class ReportTests
+    {
+        [Test]
+        public void ReportContienteUnaListaDeContenidos()
+        {
+            // arrange
+
+            // act
+            var report = new Report();
+
+            // assert
+            report.Contents.Should().Not.Be.Null();
+        }
+
+    }
+
+    public class Report
+    {
+        public object Contents
+        {
+            get
+            {
+                return new object();
+            }
+        }
+    }
+}
